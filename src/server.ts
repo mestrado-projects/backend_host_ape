6b05501d71c89d42f5da393ef6fb28b9bb769c89
Yara Tavares <yaracriistavares@gmail.com>
2024-12-06 16:12:38 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app.js";
 import "./setup.js";
+import cors from "cors";
 
 const port = process.env.PORT || 5000;
+app.use(cors());
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
