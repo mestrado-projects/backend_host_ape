7fbf6e774d62a5fc9b1b0c871039b87252f4aab8
Yara Tavares <yaracriistavares@gmail.com>
2024-12-06 16:02:23 -0300

    fix: add try catch error

--- src/controller/authGuestController.ts
+++ src/controller/authGuestController.ts
@@ -5,21 +5,35 @@ import SignInGuestUseCase from "../useCase/signInGuest.js";
 import HeadersResponseHelper from "../utils/headerResponse.js";
 
 async function signUpController(req: Request, res: Response) {
-  const user: Guests = req.body;
+  try {
+    const user: Guests = req.body;
 
-  await new SignUpGuestUseCase().execute(user);
-  res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
+    await new SignUpGuestUseCase().execute(user);
+    res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
 
-  res.sendStatus(201);
+    res.sendStatus(201);
+  } catch (error: any) {
+    console.error(error);
+    res
+      .status(500)
+      .send({ message: "Unexpected Error", details: error?.message });
+  }
 }
 
 async function signInController(req: Request, res: Response) {
-  const user: Guests = req.body;
+  try {
+    const user: Guests = req.body;
 
-  const token = await new SignInGuestUseCase().execute(user);
-  res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
+    const token = await new SignInGuestUseCase().execute(user);
+    res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
 
-  res.send({ token });
+    res.status(200).send({ token });
+  } catch (error: any) {
+    console.error(error);
+    res
+      .status(500)
+      .send({ message: "Unexpected Error", details: error?.message });
+  }
 }
 
 export { signInController, signUpController };
