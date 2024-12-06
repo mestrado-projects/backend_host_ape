d11568a3b03ce6265d6827ae63556f64934a829c
Yara Tavares <yaracriistavares@gmail.com>
2024-12-06 16:39:54 -0300

    add headers

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,19 @@ import express, { Request, Response } from "express";
 import { Apartments } from "./models/index.js";
 import dotenv from "dotenv";
 import router from "./router/index.routes.js";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 app.use(router);
 
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
 import app from "./app.js";
 import "./setup.js";
-import cors from "cors";
 
 const port = process.env.PORT || 5000;
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
