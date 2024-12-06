5e1313c3acfcfa4cdceb62759b3482d3f8e173ee
Yara Tavares <yaracriistavares@gmail.com>
2024-12-06 16:32:44 -0300

    add headers

--- src/controller/authGuestController.ts
+++ src/controller/authGuestController.ts
@@ -2,16 +2,20 @@ import { Request, Response } from "express";
 import { Guests } from "../models/index.js";
 import SignUpGuestUseCase from "../useCase/signUpGuest.js";
 import SignInGuestUseCase from "../useCase/signInGuest.js";
+import HeadersResponseHelper from "../utils/headerResponse.js";
 
 async function signUpController(req: Request, res: Response) {
   try {
     const user: Guests = req.body;
 
     await new SignUpGuestUseCase().execute(user);
+    res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
 
     res.sendStatus(201);
   } catch (error: any) {
     console.error(error);
+    res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
+
     res
       .status(500)
       .send({ message: "Unexpected Error", details: error?.message });
@@ -23,10 +27,13 @@ async function signInController(req: Request, res: Response) {
     const user: Guests = req.body;
 
     const token = await new SignInGuestUseCase().execute(user);
+    res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
 
     res.status(200).send({ token });
   } catch (error: any) {
     console.error(error);
+    res.setHeaders(HeadersResponseHelper.getInstance().getDefaultHeaders());
+
     res
       .status(500)
       .send({ message: "Unexpected Error", details: error?.message });
