2e08bdfad8b56a6344293ed9c843bbeb94640a46
Yara Tavares <yaracriistavares@gmail.com>
2024-12-06 16:35:28 -0300

    add cors 2

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,13 @@ import "./setup.js";
 import cors from "cors";
 
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
