994236228a532fdfae7c51809182f9f08d5c10b9
Gustavo Rotta <gustavo.rotta@hotmail.com>
2025-05-29 16:31:54 -0300

    remove joi-to-typescript

--- src/setup.ts
+++ src/setup.ts
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
-if (process.env.ENVIRONMENT === "local") {
-  const joiToTypescript = require("joi-to-typescript");
+// if (process.env.ENVIRONMENT === "local") {
+//   const joiToTypescript = require("joi-to-typescript");
 
-  joiToTypescript.convertFromDirectory({
-    schemaDirectory: "./src/schemas",
-    typeOutputDirectory: "./src/interfaces",
-    debug: true,
-  });
-}
+//   joiToTypescript.convertFromDirectory({
+//     schemaDirectory: "./src/schemas",
+//     typeOutputDirectory: "./src/interfaces",
+//     debug: true,
+//   });
+// }
